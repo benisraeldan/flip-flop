d880b51a7acb8205b51bcf06720280bde6b0f9d2
Shahafgo <shahaf721@gmail.com>
2018-11-02 17:38:04 +0200

    Add text to About

--- flip-flop/Controllers/HomeController.cs
+++ flip-flop/Controllers/HomeController.cs
@@ -43,7 +43,7 @@ namespace flip_flop.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "As you know, sometimes canceling a flight can be a very expensive business, while changing the name of the passenger can be much cheaper, Flip-Flop is a platform that helps people sell their flight tickets relatively cheaply so that the sellers can enjoy a substantial refund and buyers can enjoy attractive flight prices ";
 
             return View();
         }
