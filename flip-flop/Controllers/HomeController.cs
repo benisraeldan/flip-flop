f62163c65cbe8a068280baf8a96105ff0a92c593
Shahafgo <shahaf721@gmail.com>
2018-11-03 12:52:58 +0200

    Finish about page

--- flip-flop/Controllers/HomeController.cs
+++ flip-flop/Controllers/HomeController.cs
@@ -43,7 +43,7 @@ namespace flip_flop.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "As you know, sometimes canceling a flight can be a very expensive business, while changing the name of the passenger can be much cheaper, Flip-Flop is a platform that helps people sell their flight tickets relatively cheaply so that the sellers can enjoy a substantial refund and buyers can enjoy attractive flight prices ";
+            ViewData["Message"] = "As you know, sometimes canceling a flight can be a very expensive business, while changing the name of the passenger can be much cheaper, Flip-flop is a platform that helps people sell their flight tickets relatively cheaply so that the sellers can enjoy a substantial refund and buyers can enjoy attractive flight prices, have a nice flight! ";
 
             return View();
         }
